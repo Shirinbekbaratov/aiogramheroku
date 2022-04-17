8afd777d97a537d43cbf71b8086c960dae57d8e5
Shirinbek <shirinbekb@mail.ru>
2022-04-17 13:40:40 +0500

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from aiogram import Bot,Dispatcher,types
+import logging
+
+BOT_TOKEN=''
+APP_URL=''
+bot=Bot(BOT_TOKEN)
+
+dp=Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+
