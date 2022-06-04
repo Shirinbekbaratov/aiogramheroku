9a4442642cf47db5200db1b662b471fb77366cee
Shirinbek <shirinbekb@mail.ru>
2022-06-04 12:48:09 +0500

    Webhook uchun

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import os
 from aiogram import Bot,Dispatcher,executor,types
 from aiogram.types import KeyboardButton,ReplyKeyboardMarkup
 import logging
+
+from aiogram.utils.executor import start_webhook
+
 from config import BOT_TOKEN
 
 bot=Bot(BOT_TOKEN)
 
 dp=Dispatcher(bot)
+
+WEBHOOK_HOST = f'https://aiogramheroku.heroku.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+WEBAPP_HOST = f'0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT',default=8000)
 
 @dp.message_handler(commands=['start','help'])
 async def hello_message(message: types.Message):
@@ -68,7 +78,23 @@ async def changevalyut(message: types.Message):
     #await message.answer('Bajarildi')
 
 
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
-if __name__=='__main__':
-    executor.start_polling(dp,skip_updates=True)
+ if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
+
+#if __name__=='__main__':
+ #   executor.start_polling(dp,skip_updates=True)
